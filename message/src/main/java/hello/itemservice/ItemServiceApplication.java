6fb8081a0547c972db8f12dd5093b35afab55dd3
minsong0313 <57400913+minsong0313@users.noreply.github.com>
2024-03-18 13:02:01 +0900

    메시지, 국제화

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -3,6 +3,8 @@ package hello.itemservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ItemServiceApplication {
 
