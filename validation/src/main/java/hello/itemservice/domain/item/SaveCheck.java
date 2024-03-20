dcab44c58e4e29f91302963bbe6d6181b0439f57
minsong0313 <57400913+minsong0313@users.noreply.github.com>
2024-03-20 16:30:53 +0900

    V3 - Bean Validation

---
+++ validation/src/main/java/hello/itemservice/domain/item/SaveCheck.java
@@ -0,0 +1,5 @@
+package hello.itemservice.domain.item;
+
+//등록할 때 적용할 group
+public interface SaveCheck {
+}
---
+++ validation/src/main/java/hello/itemservice/domain/item/UpdateCheck.java
@@ -0,0 +1,5 @@
+package hello.itemservice.domain.item;
+
+//수정할 때 적용할 group
+public interface UpdateCheck {
+}
