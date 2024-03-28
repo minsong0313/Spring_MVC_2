b2141d2aef2e04a8997f7ca0a116723f3c374290
minsong0313 <57400913+minsong0313@users.noreply.github.com>
2024-03-28 11:39:49 +0900

    form: 스프링 부트 기본 오류 처리

--- exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
@@ -19,3 +19,8 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
 }
+
+/** 스프링 부트 기본 오류 처리
+ * 1. WebServerCustomizer 클래스 스프링 빈에 등록되지 않게 @Component 주석처리 해주고 실행해주면 끝
+ * 2. 스프링 부트가 제공하는 BasicErrorController 에 오류 처리 메서드가 구현되어 있음
+ */
